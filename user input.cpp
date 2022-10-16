1af520cbf1ecfc396e2117b4563e232946d072be
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-10-16 20:30:10 +0530

    add and sub taking user inputs

---
+++ user input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x, y;
+int sum,sub;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+sum = x + y;
+sub=x-y;
+cout << "Sum is: " << sum;
+cout << "\nSum is: " << sub;
+}
