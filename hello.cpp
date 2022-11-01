61bdfe62f8357ce88d7f4f95edb7a86a5c10cc3f
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-01 22:19:22 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,3 +4,11 @@ int main(){
   cout<<"hello shivaraj";
   return 0;
 }
+
+
+#include <iostream> 
+using namespace std; int main()
+{
+// prints hello world in C++ 
+  printf("Hello World in C++");
+}
