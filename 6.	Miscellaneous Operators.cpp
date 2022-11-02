9401431e14831c92f0e07163f60223c74f092f6d
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-02 20:07:13 +0530

    Create 6.	Miscellaneous Operators.cpp

---
+++ 6.	Miscellaneous Operators.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std; 
+int main()
+{ 
+  // variable declaration 
+  int a = 20, b = 30, c;
+
+// Largest among a and b 
+  c = (a > b) ? a : b;
+
+// Print the largest number
+cout << "Largest number between a and b is "<< c << endl; return 0;
+}
