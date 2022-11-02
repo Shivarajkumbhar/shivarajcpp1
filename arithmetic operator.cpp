2fcaab823785bd086a00710aba0295a8e0be6a65
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-02 19:37:13 +0530

    Update arithmetic operator.cpp

--- arithmetic operator.cpp
+++ arithmetic operator.cpp
@@ -16,3 +16,37 @@ cout<<(p / q);
 // modulo operator 
 cout<<(p % q);
 }
+
+
+#include<iostream> 
+using namespace std;
+int main() 
+{
+int num1 = 26; 
+int num2 = 8;
+int div = num1 / num2; 
+cout << div << endl;
+}
+
+
+#include<iostream> 
+using namespace std; 
+int main() {
+int num1 = 26.0; 
+int num2 = 8.0;
+int div = num1 / num2; 
+cout << div << endl;
+}
+
+
+#include<iostream> 
+using namespace std; 
+int main()
+{
+float num1=26.5; 
+int num2=8; cout<<num2/num1; 
+return 0;
+}
+
+
+
