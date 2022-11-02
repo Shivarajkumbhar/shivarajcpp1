01da16894e5961b03323323f73ced963e433836c
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-02 19:47:00 +0530

    Create assignment operator.cpp

---
+++ assignment operator.cpp
@@ -0,0 +1,22 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+
+int p = 10; 
+int q;
+
+// assign value using = 
+q = p;
+cout<<(q); // value of q is 10
+
+// assign value using =+ 
+q += p;
+cout<<(q); // value of q is 20
+
+
+// assign value using =*
+q *= p;
+cout<<(q); // value of q is 200
+}
+}
