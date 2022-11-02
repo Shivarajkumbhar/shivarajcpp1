83c5f24d64209fe4daa24089bf96d28579ac242e
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-02 19:41:22 +0530

    Create relational operator.cpp

---
+++ relational operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+using namespace std; 
+int main()
+{
+// create variables 
+  int p = 10, q = 15;
+// == operator
+cout<<(p == q); // false
+// != operator
+cout<<(p != q); // true
+// > operator
+cout<<(p > q); // false
+// < operator
+cout<<(p < q); // true
+// >= operator
+cout<<(p >= q); // false
+// <= operator
+cout<<(p <= q); // true
+}
